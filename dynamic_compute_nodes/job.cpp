61689fe72e8840320a541ee322de4af5644c72f9
Dave Anderson <danderson11235@gmail.com>
2021-11-06 11:30:18 -0400

    should work but havent had time to test

--- dynamic_compute_nodes/job.cpp
+++ dynamic_compute_nodes/job.cpp
@@ -3,31 +3,68 @@
 #include <string>
 #include <fstream>
 #include <cstdio>
+#include <spawn.h>
+#include <signal.h>
+#include <unistd.h>
 
-Job::Job(std::string name)
+Job::Job(std::string name, int rpi)
 {
-    std::string args;
-    std::string name;
-    system(name.c_str());
+    this->rpi = rpi;
+    this->name = name;
+    this->args = new Args();
 }
 
 Job::~Job()
 {
+    delete args;
 }
 
 Job::Job(char* prevJob) {
-    std::stringstream ss(prevJob);
-    std::string buffer;
-    std::string args;
-    std::string name;
-    getline(ss, name);
-    getline(ss, args);
-    std::ofstream outputFile;
-    outputFile.open(name);
-    while (ss >> buffer)
-    {
+    // std::stringstream ss(prevJob);
+    // std::string buffer;
+    // std::string args;
+    // std::string name;
+    // getline(ss, name);
+    // getline(ss, args);
+    // std::ofstream outputFile;
+    // outputFile.open(name);
+    // while (ss >> buffer)
+    // {
         
-    }
+    // }
     
+}
+
+void *jobLoop (void* arg) {
+    Args* myArgs = (Args*)arg;
+    int out = 0;
+    while(!myArgs->stop) {
+        sleep(1);
+        out++;
+    }
+    myArgs->out = out;
+    return nullptr;
+}
+
+void Job::startRun() {
+    pthread_t thread;
+    pthread_create(&thread, NULL, jobLoop, NULL);
+}
+
+void Job::endRun() {
+    args->stop = true;
+    sleep(2);
+    pthread_exit(NULL);
+}
+
+int Job::getVal() {
+    return args->out;
+}
+
+std::string Job::toString() {
+    std::string out = "";
+    out += name;
+    out += " " + rpi;
+    out += " " + args->out;
 }
 
--- dynamic_compute_nodes/job.hpp
+++ dynamic_compute_nodes/job.hpp
@@ -2,19 +2,31 @@
 
 #include <iostream>
 
+struct Args
+    {
+        bool stop;
+        int out;
+        Args() : stop(false), out(0){};
+    };
 class Job
 {
 private:
     int rpi;
     int hash();
+    int pid;
+    void* ret;
+    Args* args;
+    std::string name;
+    
 public:
-    Job(std::string name);
+    Job(std::string name, int rpi);
     ~Job();
     Job(char* prevJob);
 
     void startRun();
     void endRun();
-    char* send();
-    char* toString();
+    int getVal();
+    // char* send();
+    std::string toString();
 };
 
